1829352d0170ffdfbd21bf25d29be552d0f37627
ihork <js.web23@gmail.com>
2019-01-16 21:23:06 +0200

    Split server and client

--- server/dev.env.js
+++ server/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodbUri: 'mongodb://localhost/todo-api',
   secretKey: 'secretKey',
-  port: 3000
+  port: 3001
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = require('./env').port
 const bodyParser = require('body-parser')
+const path = require('path');
 
 const todoRoutes = require('./routes/todos')
 const authRoutes = require('./routes/auth')
@@ -10,12 +11,19 @@ const auth = require('./middlewares/auth')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(`${__dirname}/views`))
-app.use(express.static(`${__dirname}/public`))
+// app.use(express.static(`${__dirname}/views`))
+// app.use(express.static(`${__dirname}/public`))
 
-app.get('/', (req, res) =>
-  res.send('index.html')
-)
+// app.get('/', (req, res) =>
+//   res.send('index.html')
+// )
+if (process.env.NODE_ENV === 'production') {
+  const pathToClient = path.join(__dirname, '../client/build')
+  app.use(express.static(pathToClient));
+  app.get('/', (req, res) =>
+    res.send(pathToClient, 'index.html')
+  )
+}
 
 app.use('/api/users/:id/todos',
   auth.loginRequired, auth.ensureCorrectUser,
